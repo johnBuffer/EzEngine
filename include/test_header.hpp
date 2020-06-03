c25b3a91bc697618abfa229cd66cf5cabefa453b
Jean Tampon <jean.tampon@gmail.com>
2020-06-03 16:17:11 +0200

    bof

--- include/test_header.hpp
+++ include/test_header.hpp
@@ -3,58 +3,24 @@
 #include <vector>
 #include <functional>
 
-template<typename T>
-using Getter = T*(*)(uint64_t);
-
 
 template<typename T>
-struct ACObject
+class Component
 {
-	static T* getByIndex(uint64_t index)
+	const T& get(uint64_t id) const
 	{
-		return &objects[index];
+		return data[id];
 	}
 
-	static std::vector<T> objects;
+	static std::vector<T> data;
 };
 
 template<typename T>
-std::vector<T> ACObject<T>::objects;
+std::vector<T> Component<T>::data;
 
 
-template<typename T>
-struct ObjectPtr
+class CustomComp : public Component<CustomComp>
 {
-	ObjectPtr() = default;
-	
-	ObjectPtr(const uint64_t idx)
-		: getter(&T::getAt)
-		, index(idx)
-	{
-	}
-
-	ObjectPtr(Getter<T> get, const uint64_t idx)
-		: getter(get)
-		, index(idx)
-	{}
-
-	T* getObject()
-	{
-		return getter(index);
-	}
-
-	T* operator->()
-	{
-		return getObject();
-	}
-
-	template<typename U>
-	operator ObjectPtr<U>()
-	{
-		ObjectPtr<U> res([](uint64_t idx) { return dynamic_cast<U*>(T::getAt(idx)); }, index);
-		return res;
-	}
-
-	const Getter<T> getter;
-	const uint64_t index;
+	float truc_1;
 };
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,13 @@
 #include "test_header.hpp"
 
 
-struct GenObj
-{
-	float ok;
-};
 
-
-struct TestObj : public ACObject<TestObj>, GenObj
-{
-	float lol;
-};
 
 
 int main()
 {
-	TestObj::objects.resize(5);
-	TestObj::objects[0].ok = 8.0f;
-	TestObj::objects[0].lol = 4.0f;
-
-	ObjectPtr<TestObj> test_ptr(0);
-
-	std::cout << test_ptr->ok << std::endl;
-	std::cout << test_ptr->lol << std::endl;
-
-	ObjectPtr<GenObj> gen_ptr = test_ptr;
-
-	std::cout << gen_ptr->ok << std::endl;
+	
+	std::cout << "lol" << std::endl;
 
 	return 0;
 }
